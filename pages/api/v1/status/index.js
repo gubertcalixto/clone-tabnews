34ab5d4907912c148f7ae8ed508329ef73a3686f
Eduardo Calixto <eduardogubertcalixto@gmail.com>
2025-06-10 16:27:11 +0000

    add database + env files

--- pages/api/v1/status/index.js
+++ pages/api/v1/status/index.js
@@ -1,4 +1,8 @@
-function status(request, response) {
+import database from '../../../../infra/database.js'
+
+
+async function status(request, response) {
+  const _ = await database.query('SELECT 1;');
   response.status(200).json({ key: "value" });
 }
 
