31a987d4da427761170866c02c09a4baa2b05b9b
Eduardo Calixto <eduardogubertcalixto@gmail.com>
2025-06-23 12:56:51 +0000

    Improving in error handling in `/migrations` endpoint

--- pages/api/v1/migrations/index.js
+++ pages/api/v1/migrations/index.js
@@ -4,24 +4,27 @@ import { join } from 'node:path';
 
 async function migrations(request, response) {
   if (!['GET', 'POST'].includes(request.method)) {
-    return response.status(405).end();
+    return response.status(405).json({
+      error: `Method "${request.method}" not allow`,
+    });
   }
+
   const dryRun = request.method === 'GET';
   const dbClient = await database.getNewClient();
 
-  const migrationsApplied = await migrationRunner({
-    dbClient: dbClient,
-    dir: join('infra', 'migrations'),
-    migrationsTable: 'pgmigrations',
-    dryRun,
-    direction: 'up'
-  });
-
-  await dbClient.end();
-
-  const responseStatus = !dryRun && Boolean(migrationsApplied.length) ? 201 : 200;
-
-  return response.status(responseStatus).json(migrationsApplied);
+  try {
+    const migrationsApplied = await migrationRunner({
+      dbClient: dbClient,
+      dir: join('infra', 'migrations'),
+      migrationsTable: 'pgmigrations',
+      dryRun,
+      direction: 'up'
+    });
+    const responseStatus = !dryRun && Boolean(migrationsApplied.length) ? 201 : 200;
+    return response.status(responseStatus).json(migrationsApplied);
+  } finally {
+    await dbClient.end();
+  }
 }
 
 export default migrations;
