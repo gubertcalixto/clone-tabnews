04a97d99694e81032e7f6e24433c272839a7673f
Eduardo Calixto <eduardogubertcalixto@gmail.com>
2025-07-04 14:51:03 +0000

    test: renaming all descriptions to use new pattern

--- tests/integration/api/v1/migrations/post.test.js
+++ tests/integration/api/v1/migrations/post.test.js
@@ -13,23 +13,34 @@ async function cleanDatabase() {
   );
 }
 
-test("POST to /api/v1/migrations should return 200", async () => {
-  // First migration call, it must return all aplied migrations
-  const response = await fetch("http://localhost:3000/api/v1/migrations", {
-    method: "POST",
-  });
-  expect(response.status).toBe(201);
-  const responseBody = await response.json();
-  expect(Array.isArray(responseBody)).toBe(true);
-  expect(responseBody.length).toBeGreaterThan(0);
+describe("POST /api/v1/migrations", () => {
+  describe("Anonymous user", () => {
+    describe("running pending migrations", () => {
+      test("for the first time", async () => {
+        // First migration call, it must return all aplied migrations
+        const response = await fetch(
+          "http://localhost:3000/api/v1/migrations",
+          {
+            method: "POST",
+          },
+        );
+        expect(response.status).toBe(201);
+        const responseBody = await response.json();
+        expect(Array.isArray(responseBody)).toBe(true);
+        expect(responseBody.length).toBeGreaterThan(0);
+      });
 
-  // Second migration call, it must return an empty migrations aplied list
-  const secondResponse = await fetch(
-    "http://localhost:3000/api/v1/migrations",
-    { method: "POST" },
-  );
-  expect(secondResponse.status).toBe(200);
-  const secondResponseBody = await secondResponse.json();
-  expect(Array.isArray(secondResponseBody)).toBe(true);
-  expect(secondResponseBody.length).toBe(0);
+      test("for the second time", async () => {
+        // Second migration call, it must return an empty migrations aplied list
+        const secondResponse = await fetch(
+          "http://localhost:3000/api/v1/migrations",
+          { method: "POST" },
+        );
+        expect(secondResponse.status).toBe(200);
+        const secondResponseBody = await secondResponse.json();
+        expect(Array.isArray(secondResponseBody)).toBe(true);
+        expect(secondResponseBody.length).toBe(0);
+      });
+    });
+  });
 });
