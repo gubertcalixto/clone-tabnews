0151899b4241ce4d03c99bdd182cc9b1ceeb5659
Eduardo Calixto <eduardogubertcalixto@gmail.com>
2025-07-04 13:25:41 -0300

    Maintenance: `dependencies` + `refactoring` (#21)
    * build: updating `package.json` versions
    
    * refactor: rename `wait-for-postgres` script to `services:wait:database`
    
    * refactor: rename `migration` script to `migrations`
    
    * refactor: move `test` scripts to top
    
    * feat: add `posttest` script
    
    * chore: add `verbose` param to `test:watch` script
    
    * test: renaming all descriptions to use new pattern
    
    * refactor: use `orchestrator.clearDatabase()` in tests
    
    * fix: change `join` to `resolve` in `migrations` endpoint
    
    * style: prettier format to `orchestrator.js`

--- tests/integration/api/v1/migrations/post.test.js
+++ tests/integration/api/v1/migrations/post.test.js
@@ -1,35 +1,39 @@
 require("dotenv").config({ path: ".env.development" });
-import database from "infra/database";
 import orchestrator from "tests/orchestrator";
 
 beforeAll(async () => {
   await orchestrator.waitForAllServices();
-  await cleanDatabase();
+  await orchestrator.clearDatabase();
 });
 
-async function cleanDatabase() {
-  await database.query(
-    "DROP SCHEMA IF EXISTS public CASCADE; CREATE SCHEMA public",
-  );
-}
+describe("POST /api/v1/migrations", () => {
+  describe("Anonymous user", () => {
+    describe("running pending migrations", () => {
+      test("for the first time", async () => {
+        // First migration call, it must return all aplied migrations
+        const response = await fetch(
+          "http://localhost:3000/api/v1/migrations",
+          {
+            method: "POST",
+          },
+        );
+        expect(response.status).toBe(201);
+        const responseBody = await response.json();
+        expect(Array.isArray(responseBody)).toBe(true);
+        expect(responseBody.length).toBeGreaterThan(0);
+      });
 
-test("POST to /api/v1/migrations should return 200", async () => {
-  // First migration call, it must return all aplied migrations
-  const response = await fetch("http://localhost:3000/api/v1/migrations", {
-    method: "POST",
+      test("for the second time", async () => {
+        // Second migration call, it must return an empty migrations aplied list
+        const secondResponse = await fetch(
+          "http://localhost:3000/api/v1/migrations",
+          { method: "POST" },
+        );
+        expect(secondResponse.status).toBe(200);
+        const secondResponseBody = await secondResponse.json();
+        expect(Array.isArray(secondResponseBody)).toBe(true);
+        expect(secondResponseBody.length).toBe(0);
+      });
+    });
   });
-  expect(response.status).toBe(201);
-  const responseBody = await response.json();
-  expect(Array.isArray(responseBody)).toBe(true);
-  expect(responseBody.length).toBeGreaterThan(0);
-
-  // Second migration call, it must return an empty migrations aplied list
-  const secondResponse = await fetch(
-    "http://localhost:3000/api/v1/migrations",
-    { method: "POST" },
-  );
-  expect(secondResponse.status).toBe(200);
-  const secondResponseBody = await secondResponse.json();
-  expect(Array.isArray(secondResponseBody)).toBe(true);
-  expect(secondResponseBody.length).toBe(0);
 });
--- tests/integration/api/v1/status/get.test.js
+++ tests/integration/api/v1/status/get.test.js
@@ -4,23 +4,30 @@ beforeAll(async () => {
   await orchestrator.waitForAllServices();
 });
 
-test("GET to /api/v1/status should return 200", async () => {
-  const response = await fetch("http://localhost:3000/api/v1/status");
-  expect(response.status).toBe(200);
-  const responseBody = await response.json();
+describe("GET /api/v1/status", () => {
+  describe("Anonymous user", () => {
+    test("retrieving current system status", async () => {
+      const response = await fetch("http://localhost:3000/api/v1/status");
+      expect(response.status).toBe(200);
+      const responseBody = await response.json();
 
-  expect(responseBody.updated_at).toBeDefined();
-  const parsedUpdatedAt = new Date(responseBody.updated_at).toISOString();
-  expect(responseBody.updated_at).toEqual(parsedUpdatedAt);
+      expect(responseBody.updated_at).toBeDefined();
+      const parsedUpdatedAt = new Date(responseBody.updated_at).toISOString();
+      expect(responseBody.updated_at).toEqual(parsedUpdatedAt);
 
-  expect(responseBody.dependencies.database.version).toEqual("16.0");
+      expect(responseBody.dependencies.database.version).toEqual("16.0");
 
-  const pgMaxConnections = responseBody.dependencies.database.max_connections;
-  expect(pgMaxConnections).toBe(100);
+      const pgMaxConnections =
+        responseBody.dependencies.database.max_connections;
+      expect(pgMaxConnections).toBe(100);
 
-  expect(responseBody.dependencies.database.opened_connections).toBeDefined();
-  const pgConnectionsCount = parseInt(
-    responseBody.dependencies.database.opened_connections,
-  );
-  expect(pgConnectionsCount).toEqual(1);
+      expect(
+        responseBody.dependencies.database.opened_connections,
+      ).toBeDefined();
+      const pgConnectionsCount = parseInt(
+        responseBody.dependencies.database.opened_connections,
+      );
+      expect(pgConnectionsCount).toEqual(1);
+    });
+  });
 });
